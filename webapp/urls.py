5bdb9e74bfcbd31b5c7b864fca44ed3f30b860d9
mudabir94 <mudabirahmad1994@gmail.com>
2018-10-09 07:47:41 -0700

    added some new models

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.urls import path
 from . import views
-
 urlpatterns = [
     # These urls doesn't have templates. These functions are
     # called  through ajax. They are either used to retrieve  
@@ -10,21 +9,16 @@ urlpatterns = [
     url(r'^ind/',views.ind, name='ind'),                   
     url(r'^on/',views.on, name='on'),                       
     url(r'^globalFunc/',views.globalFunc,name='gf'),      
-    
- 
-                                                           
-    
-    
+#----------------------------------------------------------------------------------------------------------    
+    # Basic Landing Pages
+    # Default page / Home page. 
+    url(r'^$', views.Home.as_view(), name='homepage'), 
     
     url(r'^admin_setup/',views.adminSetup, name='admin_setup'),
-    url(r'^$', views.signUp, name='signup'), 
-   
-    url(r'^blog/', views.blogview.as_view(), name='blog'), 
     url(r'^mobile/',views.mobile_phone_view.as_view(),name='mobileview'),
     path('mobile_info/<int:id>',views.mobile_phone_view.one_mobile_func, name='mobileinfo'),
     url(r'^filter/',views.filter.as_view(), name='filter'),
-    #this page is not used anywhere now.
-    #url(r'^showfilter/',views.showFilter.as_view(), name='showfilter'),
+    
     
     url(r'^cart/',views.cart, name='cart'),
     url(r'^showmob/',views.showMob, name='sm'),
@@ -36,5 +30,11 @@ urlpatterns = [
 
     url(r'^biastestfeatures/',views.BiasTestFeature.as_view(), name='select_biasfeature'),
     url(r'^manageshortlist/',views.ManageShortList.as_view(), name='manage_shortlisting'),
+#---------------------------------------------------------------------------------------------   
+    ## Pages that are not currently used. 
+    #url(r'^blog/', views.blogview.as_view(), name='blog'), 
+    #url(r'^showfilter/',views.showFilter.as_view(), name='showfilter'),
+    #url(r'^$', views.signup, name='signup'), 
 
+#---------------------------------------------------------------------------------------------
 ]
