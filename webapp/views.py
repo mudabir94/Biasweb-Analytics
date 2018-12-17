d422720d8c7d9622ecc1af562643059cf6eaacb0
mudabir94 <mudabirahmad1994@gmail.com>
2018-12-17 01:42:22 -0800

    todos in Ahp2by2_worflow

--- webapp/views.py
+++ webapp/views.py
@@ -159,7 +159,7 @@ def storeSelectedAdminPhones(request):
 
            
             userobj=User.objects.get(pk=request.user.id)
-            expobj=exp.objects.get(pk=178)
+            expobj=exp.objects.get(pk=175)
             smgphone=samsung_phone.objects.get(pk=mobiledata_json)
             selphones=selectedAdminPhones(user=userobj,exp=expobj,mob=smgphone)
             selphones.save()
