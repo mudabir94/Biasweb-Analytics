0eb83d17622746b7ed76dbaf1abdc903a136084f
mudabir94 <mudabirahmad1994@gmail.com>
2018-12-31 03:02:05 -0800

    import-export functionality in some of the tables.

--- webapp/admin.py
+++ webapp/admin.py
@@ -14,8 +14,7 @@ from .models import Subject,experiment_feature,Batch,Block,prunedmobilephones
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
-class RoleAdmin(admin.ModelAdmin):
-    list_display= ('id', 'role_name')
+
 class MyUserAdmin(UserAdmin):
     form = MyUserChangeForm
     fieldsets = UserAdmin.fieldsets + (
@@ -37,35 +36,71 @@ class templateRoleAdmin(admin.ModelAdmin):
     'can_add','can_view','can_del',\
     'created_at','update_at',)    
 
-class PlatformFeatureAdmin(admin.ModelAdmin):
-    list_display= ('id', 'feature_name','feature_symbol','feature_levels')
 class ExperimentAdmin(admin.ModelAdmin):
     list_display= ('id','custom_exp_id')
+#----------------------------------------------------------------------------
 
+# platform_feature resource
+class platformResource(resources.ModelResource):
+    class meta:
+        model=platform_feature
+#platform_feature admin
+class PlatformFeatureAdmin(ImportExportActionModelAdmin):
+    list_display= ('id', 'feature_name','feature_symbol','feature_levels')
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    
+#----------------------------------------------------------------------------
+
+# samsung_phone Resource
 class samsung_phone_Resource(resources.ModelResource):
     class meta:
         model=samsung_phone
-
+# samsung_phone admin
 class SamsungAdmin(ImportExportActionModelAdmin):
     pass
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    
+#----------------------------------------------------------------------------
+
+class roleResource(resources.ModelResource):
+    class meta:
+        model=Role
+class RoleAdmin(ImportExportActionModelAdmin):
+    list_display= ('id', 'role_name')
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    
+#----------------------------------------------------------------------------
+class selectedAdminPhonesResource(resources.ModelResource):
+    class meta:
+        model=selectedAdminPhones
+class selectedAdminPhones_Admin(ImportExportActionModelAdmin):
+    pass
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    
+#----------------------------------------------------------------------------
+
+
+
+
 admin.site.register(User, MyUserAdmin)
-admin.site.register(signup_table)
-admin.site.register(blog)
-admin.site.register(mobile_phone)
-admin.site.register(phone)
+
 admin.site.register(samsung_phone,SamsungAdmin)
-admin.site.register(sort_feature)
-admin.site.register(userscoreRecord)
 admin.site.register(Role,RoleAdmin)
 admin.site.register(platform_feature,PlatformFeatureAdmin)
 admin.site.register(experiment,ExperimentAdmin)
 admin.site.register(templates,templateAdmin)
 admin.site.register(template_roles,templateRoleAdmin)
+admin.site.register(selectedAdminPhones,selectedAdminPhones_Admin)
+
+admin.site.register(sort_feature)
+admin.site.register(userscoreRecord)
+admin.site.register(signup_table)
+admin.site.register(blog)
+admin.site.register(mobile_phone)
+admin.site.register(phone)
 admin.site.register(Subject)
 admin.site.register(experiment_feature)
 admin.site.register(Batch)
 admin.site.register(Block)
 admin.site.register(prunedmobilephones)
 admin.site.register(feature)
-admin.site.register(selectedAdminPhones)
